1ab07f51de7c43b7789d69d9149cc02699d8fc17
Martin Ahrer <this@martinahrer.at>
2015-10-11 20:59:55 +0200

    Added docker volumes

--- src/main/docker/docker-volumes.sh
+++ src/main/docker/docker-volumes.sh
@@ -1,7 +1,20 @@
-docker create --name ubuntu-container -v /opt/database ubuntu:14.04 /bin/bash
-docker inspect container ubuntu-container
-docker rm ubuntu-container
+# Run in virtualbox/vmware!
 
+# Exercise 1
+docker create --name data-container -v /opt/database ubuntu:14.04 /bin/bash
+docker inspect container data-container
+docker rm data-container
+
+# Exercise 2
 mkdir $HOME/docker
-docker run --name ubuntu-container -v $HOME/docker:/opt/database ubuntu:14.04 /bin/bash -c "touch /opt/database/touched.txt"
+docker run --name data-container --rm -v $HOME/docker:/opt/database ubuntu:14.04 /bin/bash -c "touch /opt/database/touched.txt"
 ll $HOME/docker
+rm $HOME/docker/touched.txt
+
+# Exercise 3
+docker create --name data-container -v /opt/database ubuntu:14.04 /bin/bash
+# Note that we didn't start data-container
+
+docker run --rm --name server-container --volumes-from data-container ubuntu:14.04 /bin/bash -c "touch /opt/database/touched.txt"
+docker run --rm --name server-container --volumes-from data-container ubuntu:14.04 /bin/bash -c "ls /opt/database"
+sudo ls `docker inspect --format='{{(index .Mounts 0).Source}}' data-container`
